58b82e5506361fc88e33a9e57f8f9587235df9d9
Gustiagung19 <75777108+Gustiagung19@users.noreply.github.com>
2021-06-09 00:38:57 +0800

    Delete item-kendaraan directory

--- item-kendaraan/item-kendaraan.lua
+++
@@ -1,9 +0,0 @@
-ESX.RegisterUsableItem('NAMA_ITEM', function(source)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local name = GetHashKey("NAMA_SPAWN")
-
-    xPlayer.removeInventoryItem('NAMA_ITEM', 1)
-
-    TriggerClientEvent('esx:spawnVehicle', source, name)
-    TriggerClientEvent('esx:showNotification', source, 'spawned 1x ?')
-end)
